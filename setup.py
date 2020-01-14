17eb4c3fcdf2d74b1622f76eaabea6dca7af942b
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2020-01-14 17:36:07 +0900

    support latest Python

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ import setup_boilerplate
 
 
 class Package(setup_boilerplate.Package):
-
     """Package metadata."""
 
     name = 'slack-bots'
@@ -20,6 +19,8 @@ class Package(setup_boilerplate.Package):
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Utilities']
     keywords = ['bot', 'bots', 'chat', 'slack']
